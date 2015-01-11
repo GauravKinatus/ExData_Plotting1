cb7d0d4a2509aa44c8f3efb49e181ce727e57948
Gaurav Garg <gaurav_garg@yahoo.com>
2015-01-10 16:42:35 -0800

    Changed the background to transparent

--- Plot2.R
+++ Plot2.R
@@ -3,19 +3,18 @@
 
 ## part 2 - Plot2.R
         # set the working directory to the course directory
-        setwd("../Exploratory Data Analysis")
+        setwd("../Exploratory Data Analysis/ExData_Plotting1")
         # read the file with header Note: ';' is the seperator and '?' is NA
         datafile <- "household_power_consumption.txt"
-        df <- read.csv(datafile, header = TRUE, sep = ";", na.strings = "?", 
-                       colClasses = c("Date","numeric","numeric","numeric","numeric","numeric",
-                                      "numeric","numeric","numeric"))
+        df <- read.csv(datafile, header = TRUE, sep = ";", na.strings = "?")
+        # add a column merging the date and time columns 
         df$datetime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S") 
         # subset the data frame for the target date range
         target.df <- subset(df, (datetime >= "2007-02-01 00:00:00" & datetime <="2007-02-02 23:59:59"))
         # setup the histogram and plot it in the file
-        png(filename = "plot2.png", width = 480, height = 480, units = "px")
-        plot(target.df$datetime, target.df$Global_active_power, type="n", xlab="",
-             ylab="Global Active Power (kilowatts)" )
+        png(filename = "./figure/plot2.png", width = 480, height = 480, units = "px", bg="transparent")
+        plot(target.df$datetime, target.df$Global_active_power, type="l", xlab="",
+             ylab="Global Active Power (kilowatts)", bg="transparent" )
         lines(target.df$datetime, target.df$Global_active_power, type="l",  col="black", pch = 0 ) 
         dev.off()
         
--- Plot3.R
+++ Plot3.R
@@ -3,16 +3,12 @@
 
 ## part 3 - Plot3.R
         # set the working directory to the course directory
-        setwd("../Exploratory Data Analysis")
+        setwd("../Exploratory Data Analysis/ExData_Plotting1")
         # read the file with header Note: ';' is the seperator and '?' is NA
         datafile <- "household_power_consumption.txt"
         df <- read.csv(datafile, header = TRUE, sep = ";", na.strings = "?")
       
-       # df <- read.csv(datafile, header = TRUE, sep = ";", na.strings = "?", 
-       #                colClasses = c("Date","character","numeric","numeric","numeric","numeric",
-       #                               "numeric","numeric","numeric"))
-        # convert the first column of the data frame to Date (vs Fractor)
-        #df[,1] <- as.Date(df[,1])
+        # add a column merging the date and time columns 
         df$datetime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S") 
         # subset the data frame for the target date range
         target.df <- subset(df, (datetime >= "2007-02-01 00:00:00" & datetime <="2007-02-02 23:59:59"))
@@ -20,9 +16,9 @@
         yRange <- (target.df$Sub_metering_1)
         
         # setup the histogram and plot it in the file
-        png(filename = "plot3.png", width = 480, height = 480, units = "px")
+        png(filename = "./figure/plot3.png", width = 480, height = 480, units = "px", bg = "transparent")
         plot(target.df$datetime, target.df$Sub_metering_1, type="n", xlab="",
-             ylab="Energy sub metering" )
+             ylab="Energy sub metering", bg="transparent" )
         lines(target.df$datetime, target.df$Sub_metering_1, type="l",  col="black", pch = 0 ) 
         lines(target.df$datetime, target.df$Sub_metering_2, type="l",  col="red", pch = 0 ) 
         lines(target.df$datetime, target.df$Sub_metering_3, type="l", col="blue", pch = 0 ) 
