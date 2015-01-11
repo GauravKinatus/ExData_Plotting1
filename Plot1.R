c97d6496fd54c97719bcca47c8406a1ce8350a40
Gaurav Garg <gaurav_garg@yahoo.com>
2015-01-10 16:32:30 -0800

    Changed the directory structure to match

--- Plot1.R
+++ Plot1.R
@@ -3,19 +3,18 @@
 
 ## part 1 - Plot1.R
         # set the working directory to the course directory
-        setwd("../Exploratory Data Analysis")
+        setwd("../Exploratory Data Analysis/ExData_Plotting1")
         # read the file with header Note: ';' is the seperator and '?' is NA
         datafile <- "household_power_consumption.txt"
-        df <- read.csv(datafile, header = TRUE, sep = ";", na.strings = "?", 
-                       colClasses = c("Date",strptime,"numeric","numeric","numeric","numeric",
-                                      "numeric","numeric","numeric"))
-        # convert the first column of the data frame to Date (vs Fractor)
-        df[,1] <- as.Date(df[,1])
+        df <- read.csv(datafile, header = TRUE, sep = ";", na.strings = "?")
+        # add a column merging the date and time columns 
+        df$datetime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S") 
         # subset the data frame for the target date range
-        target.df <- subset(df, ((Date == as.Date("02/01/2007")) | (Date == as.Date("02/02/2007"))))
+        target.df <- subset(df, (datetime >= "2007-02-01 00:00:00" & datetime <="2007-02-02 23:59:59"))
+
         # setup the histogram and plot it in the file
-        png(filename = "plot1.png", width = 480, height = 480, units = "px", background = "transparent")
+        png(filename = "./figure/plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
         hist(target.df$Global_active_power, col="red", main ="Global Active Power", 
-             xlab ="Global Active Power (kilowatts)" )
+             xlab ="Global Active Power (kilowatts)", breaks = 15 )
         dev.off()
         
